442b86b5f3c905fe2bf044b6e94835d4fe6a65f7
Vedant-Patil <vedantnp200@outlook.com>
2023-09-01 15:16:22 +0530

    Insert, Update and Delete operations are programmed.

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bank_Transactions.Models
@@ -9,17 +10,31 @@ namespace Bank_Transactions.Models
         public int TransactionId { get; set; }
 
         [Column(TypeName ="nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required(ErrorMessage = "This field is required!")]
+        [StringLength(12, MinimumLength = 12, ErrorMessage = "Maximum 12 characters required.")]
         public string AccountNumber { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Beneficiary Name")]
+        [Required(ErrorMessage = "This field is required!")]
         public string BeneficiaryName { get; set; }
 
         [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        [Required(ErrorMessage = "This field is required!")]
         public string BankName { get; set; }
 
         [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("SWIFT Code")]
+        [Required(ErrorMessage = "This field is required!")]
+        [StringLength(11, MinimumLength = 11, ErrorMessage = "Maximum 12 characters required.")]
         public string SWIFTCode { get; set; }
+
+        [Required(ErrorMessage = "This field is required!")]
         public int Amount { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd-MMM-yyyy}")]
         public DateTime Date { get; set; }
 
     }
